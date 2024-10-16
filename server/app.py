f1b052b2e21f0624cc789fb608839a020972927e
baimet <isaacbaimetyatich@gmail.com>
2024-10-16 07:22:02 +0300

    cookies sessions lab done

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, jsonify, session
 from flask_migrate import Migrate
 
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -22,13 +22,30 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    return jsonify([article.to_dict() for article in articles])
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Set the initial value of session['page_views'] if it doesn't exist
+    session['page_views'] = session.get('page_views', 0)
+    
+    # Increment the page view count
+    session['page_views'] += 1
 
-    pass
+    # Check if page views are within the allowed limit
+    if session['page_views'] > 3:
+        # If the user has viewed more than 3 articles, return an error message
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+
+    # If within the limit, return the article
+    article = Article.query.get(id)
+    if article:
+        return jsonify(article.to_dict())
+    else:
+        return jsonify({'message': 'Article not found'}), 404
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
